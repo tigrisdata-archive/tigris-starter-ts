b543b131dbb75f86386dfb244e8941d5d33af918
Jigar Joshi <jigarjm@gmail.com>
2022-06-15 16:39:20 -0700

    fix: By mistake added createOrder handler

--- src/controllers/order-controller.ts
+++ src/controllers/order-controller.ts
@@ -2,8 +2,6 @@ import express, {NextFunction, Request, Response, Router} from "express";
 import {Collection, DB} from "@tigrisdata/core";
 import {Order} from "../models/order";
 import {Controller} from "./controller";
-import {User} from "../models/user";
-import {Product} from "../models/product";
 
 export class OrderController implements Controller {
 
@@ -41,84 +39,6 @@ export class OrderController implements Controller {
             res.status(200).json(response);
         }).catch(error => {
             next(error);
-        });
-    };
-    public createOrder = async (req: Request, res: Response) => {
-        this.db.transact(async tx => {
-            // get user
-            const user: (User | undefined) = await this.db.getCollection<User>('users').readOne({
-                userId: Number.parseInt(req.params.userId)
-            }, tx);
-            if (user === undefined) {
-                res.status(404).json({error: 'User not found'});
-                return;
-            }
-
-            // get product
-            const product: (Product | undefined) = await this.db.getCollection<Product>('products').readOne({
-                    productId: Number.parseInt(req.params.productId)
-                },
-                tx
-            );
-            if (product === undefined) {
-                res.status(404).json({error: 'Product not found'});
-                return;
-            }
-
-            // read quantity for order
-            const qty: number = Number.parseInt(req.params.quantity);
-
-            // check quantity available
-            if (qty > product.quantity) {
-                res.status(412).json({error: 'Insufficient product quantity'});
-                return;
-            }
-            const orderTotal: number = qty * product.price;
-
-            // check balance available
-            if (user.balance < orderTotal) {
-                res.status(412).json({error: 'Insufficient user balance'});
-                return;
-            }
-
-            // deduct balance
-            await this.db.getCollection<User>('users').update(
-                {
-                    userId: user.userId
-                },
-                {
-                    balance: user.balance - orderTotal
-                },
-                tx
-            );
-            console.log('deducted user balance');
-
-            // deduct product quantity
-            await this.db.getCollection<Product>('products').update(
-                {
-                    productId: product.productId
-                },
-                {
-                    quantity: product.quantity - qty
-                },
-                tx
-            );
-            // create order
-            const order: Order = {
-                orderTotal: orderTotal,
-                userId: user.userId,
-                productItems: [
-                    {
-                        productId: product.productId,
-                        quantity: qty
-                    }
-                ]
-            };
-
-            await this.orders.insert(order, tx);
-            console.log('order created');
-
-            res.status(200).json({status: 'Order placed successfully'});
         });
     };
 
